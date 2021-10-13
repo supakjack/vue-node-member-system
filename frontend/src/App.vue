1fb85c2a1b45a124be22e56e5dd32502b7db047f
Supak Pukdam <48548611+supakjack@users.noreply.github.com>
2021-10-13 21:04:29 +0700

    add navbar
    add navbar

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/layouts/Navbar.vue";
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
